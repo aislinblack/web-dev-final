c78949fdedc7a101dff472b65643fc711aa5bfa6
aislinblack <1of4black@gmail.com>
2022-04-14 15:20:04 -0400

    Add bootstrap and font awesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import Home from './components/home';
+
 function App() {
-  return <div className='App'>hi</div>;
+  return (
+    <BrowserRouter>
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
